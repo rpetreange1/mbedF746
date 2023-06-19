f7bcec23b0009787b0e18c766399fd673a6866bb
rpetreange1 <raoul.petrean@u-psud.fr>
2023-06-20 01:07:40 +0200

    Tout marche obstacle a ameliorer

--- src/main.cpp
+++ src/main.cpp
@@ -17,57 +17,10 @@ DigitalIn  btnDroite(PF_8);
 DigitalIn  btnBas(PA_8);
 DigitalIn  btnGauche(PB_14);
 
-static void event_cb(lv_event_t * e)
-    {
-        lv_obj_t * obj = lv_event_get_target(e);
-        LV_UNUSED(obj);
-        LV_LOG_USER("Button %s clicked", lv_msgbox_get_active_btn_text(obj));
-    }
-
-void msgbox_start(void)
-    {
-        static const char * btns[] = {"Apply", "Close", ""};
-
-        lv_obj_t * mbox1 = lv_msgbox_create(NULL, "Hello", "nigga", btns, true);
-        lv_obj_add_event_cb(mbox1, event_cb, LV_EVENT_VALUE_CHANGED, NULL);
-        lv_obj_center(mbox1);
-    } 
-
-// Déclare les 3 images pour l'animation de la voiture 
-    LV_IMG_DECLARE(voiture1)
-    LV_IMG_DECLARE(voiture2)
-    LV_IMG_DECLARE(voiture3)
-    
-// Met les 3 images dans un tableau pour pouvoir les utiliser de façon plus dynamique
-static const lv_img_dsc_t * anim_imgs[3] = {
-    &voiture1,
-    &voiture2,
-    &voiture3,
-    };
-
-// Fonction pour l'animation de la voiture 
-void lv_anim_voiture(void)
-{
-    lv_obj_t * animvoit = lv_animimg_create(lv_scr_act());
-    lv_obj_center(animvoit);
-    lv_animimg_set_src(animvoit, const_cast<lv_img_dsc_t **>(anim_imgs), 3);
-    lv_img_set_zoom(animvoit, 96);
-    lv_img_set_angle(animvoit, 900);
-    lv_obj_align(animvoit, LV_ALIGN_CENTER, -150, 0);
-    lv_obj_set_size(animvoit, 210, 124);
-    lv_animimg_set_duration(animvoit, 500);
-    lv_animimg_set_repeat_count(animvoit, LV_ANIM_REPEAT_INFINITE);
-    lv_animimg_start(animvoit);
-}
-
 int main() {
     threadLvgl.lock();
     
     lv_obj_clear_flag(lv_scr_act(), LV_OBJ_FLAG_SCROLLABLE); // Désactive la scrollbar et empêche l'utilisateur de scrollé
-    
-    // Déclarer mbox1 ici sinon erreur
-    lv_obj_t * mbox1;
-    msgbox_start();
 
     /*Style de la ligne*/
     lv_point_t line_points_taille1[] = {{0,0}, {0, 10}}; // Ligne qui voit sa largeur s'incrémenter le static a été enlevé pour modifier la valeur
@@ -76,6 +29,8 @@ int main() {
     int pos_x_arbre = 220;  // Position de l'arbre a son apparition sur l'écran
     int largeur_arbre = 80; // Largeur de l'arbre au début 
     int pos_X_voiture = 0;
+    int pos_x_obs = 0;
+    int pos_y_obs = 0;
     int Direction = 0;
     lv_style_init(&style_line);
     lv_style_set_line_width(&style_line, 3);
@@ -113,6 +68,25 @@ int main() {
     lv_obj_align(imgtree, LV_ALIGN_OUT_LEFT_MID, pos_x_arbre, 130);
     lv_obj_set_size(imgtree, 134, 207);
 
+    /*Met en place l'image de l'obstacle drapeau*/
+    LV_IMG_DECLARE(obstacles);
+    lv_obj_t * imgobst = lv_img_create(lv_scr_act());
+    lv_img_set_src(imgobst, &obstacles);
+    lv_img_set_zoom(imgobst, 128);
+    lv_img_set_angle(imgobst, 900);
+    lv_obj_align(imgobst, LV_ALIGN_CENTER, pos_y_obs, pos_x_obs);
+    lv_obj_set_size(imgobst, 63, 51);
+
+    /*Met en place l'image de l'écran d'accueil*/
+    LV_IMG_DECLARE(press_start);
+    lv_obj_t * imgstart = lv_img_create(lv_scr_act());
+    lv_img_set_src(imgstart, &press_start);
+    lv_img_set_zoom(imgstart, 256);
+    lv_img_set_angle(imgstart, 900);
+    lv_obj_align(imgstart, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_size(imgstart, 266, 33);
+    lv_obj_add_flag(imgobst, LV_OBJ_FLAG_HIDDEN);
+
     threadLvgl.unlock();
 
     while (1) {
@@ -123,8 +97,10 @@ int main() {
 
         lv_obj_align(line, LV_ALIGN_CENTER, pos_x_ligne,0);  // Pour que la ligne bouge 
         lv_obj_align(imgtree, LV_ALIGN_OUT_LEFT_MID, pos_x_arbre, 130);
+        lv_obj_align(imgobst, LV_ALIGN_CENTER, pos_x_obs,pos_X_voiture);
         pos_x_ligne = pos_x_ligne -10;  // On fait reculer la ligne de 10
         pos_x_arbre = pos_x_arbre -10;  // On fait reculer la ligne de 10
+        pos_x_obs = pos_x_obs -10;  // On fait reculer l'obstacle de 10
         line_points_taille1[1].y += 10;  // La largeur de la ligne augmente de 
         largeur_arbre += 10;  // La largeur de l'arbre augmente de 10
         lv_img_set_zoom(imgtree, largeur_arbre);
@@ -134,8 +110,10 @@ int main() {
         {
             lv_obj_align(line, LV_ALIGN_CENTER, pos_x_ligne,0);  // Pour que la ligne bouge 
             lv_obj_align(imgtree, LV_ALIGN_OUT_LEFT_MID, pos_x_arbre, 130);
+            lv_obj_align(imgobst, LV_ALIGN_CENTER, pos_x_obs,pos_X_voiture-2);
             pos_x_arbre = pos_x_arbre -10;  // On fait reculer la ligne de 10
             pos_x_ligne = pos_x_ligne -10;  // On fait reculer la ligne de 10
+            pos_x_obs = pos_x_obs -10;  // On fait reculer l'obstacle de 10
             line_points_taille1[1].y += 10;  // La largeur de la ligne augmente de 10
             largeur_arbre += 10;  // La largeur de l'arbre augmente de 10
             lv_img_set_zoom(imgtree, largeur_arbre);
@@ -145,8 +123,10 @@ int main() {
         {
             lv_obj_align(line, LV_ALIGN_CENTER, pos_x_ligne,0);  // Pour que la ligne bouge 
             lv_obj_align(imgtree, LV_ALIGN_OUT_LEFT_MID, pos_x_arbre, 130);
+            lv_obj_align(imgobst, LV_ALIGN_CENTER, pos_x_obs,pos_X_voiture-3);
             pos_x_arbre = pos_x_arbre -10;  // On fait reculer l'arbre de 10
             pos_x_ligne = pos_x_ligne -10;  // On fait reculer la ligne de 10
+            pos_x_obs = pos_x_obs -10;  // On fait reculer l'obstacle de 10
             line_points_taille1[1].y += 10;  // La largeur de la ligne augmente de 10
             largeur_arbre += 10;  // La largeur de l'arbre augmente de 10
             lv_img_set_zoom(imgtree, largeur_arbre);
@@ -156,9 +136,11 @@ int main() {
         {
             pos_x_ligne = 8;
             pos_x_arbre = 220;
+            pos_x_obs = 0;
             largeur_arbre = 80;  // La largeur de l'arbre est remise à sa valeur initiale
             lv_obj_align(line, LV_ALIGN_CENTER, pos_x_ligne,0);  // Pour que la ligne bouge 
             lv_obj_align(imgtree, LV_ALIGN_OUT_LEFT_MID, pos_x_arbre, 130);
+            lv_obj_align(imgobst, LV_ALIGN_CENTER, pos_x_obs,pos_X_voiture-5);
             line_points_taille1[1].y = 10;  // La largeur de la ligne retourne à sa valeur initiale
             largeur_arbre += 10;  // La largeur de l'arbre augmente de 10
             lv_img_set_zoom(imgtree, largeur_arbre);
@@ -197,7 +179,8 @@ int main() {
             break;
 
             case Bas:
-            lv_msgbox_close(mbox1);
+            lv_obj_add_flag(imgstart, LV_OBJ_FLAG_HIDDEN);  // Fait disparaitre le PRESS START
+            lv_obj_clear_flag(imgobst, LV_OBJ_FLAG_HIDDEN); // Fait apparaitre le drapeau
             Direction = 0;
             break;
 
